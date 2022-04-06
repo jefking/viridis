e76d6cf4fb1f6e34bb36fd9ef338a35191a3510f
Jef King <jbdking@pm.me>
2022-04-06 16:49:11 -0700

    display

--- display/index.js
+++ display/index.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
+
+const html = fs.readFileSync('./display/index.htm','utf-8')
+
 module.exports = async function (context, req) {
-    const responseMessage = "<html></html>";
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage,
+        body: html,
         headers: {
             'Content-Type': 'text/html; charset=utf-8'
         }
