a3214e7c24c6406b1ecacd87912aebc976ce04a2
Jef King <jbdking@pm.me>
2022-04-10 09:58:53 -0700

    a few coordinates

---
+++ math/index.js
@@ -0,0 +1,20 @@
+
+
+
+let places = [{
+    lat: 49.255320,
+    long: -123.246590,
+    name: 'UBC',
+},{
+    lat: 49.269510,
+    long: -123.233840,
+    name: 'spanish banks',
+},{
+    lat: 49.270474,
+    long: -123.148580,
+    name: 'home',
+},{
+    lat: 49.271096,
+    long: -123.133962,
+    name: 'granville island',
+}];
